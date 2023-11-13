db620a723718a843ffcf8435386360d38797f2ed
Luciano Alves <lasbr_alves@outlook.com>
2023-11-13 14:09:35 -0300

    Project started; docker-compose.yml and application.properties configured

---
+++ src/main/java/br/com/lasbr/adopetapi/AdopetapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.lasbr.adopetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class AdopetapiApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(AdopetapiApplication.class, args);
+        }
+    }
---
+++ src/test/java/br/com/lasbr/adopetapi/AdopetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lasbr.adopetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopetapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
