913f391266f09a00c32ebc6246e5f891e8aae94f
Luciano Alves <lasbr_alves@outlook.com>
2023-11-14 22:30:09 -0300

    feat: added dto PetResponse

---
+++ src/main/java/br/com/lasbr/adopetapi/dto/PetResponse.java
@@ -0,0 +1,21 @@
+package br.com.lasbr.adopetapi.dto;
+
+import br.com.lasbr.adopetapi.entity.Pet;
+import br.com.lasbr.adopetapi.entity.PetType;
+
+    public record PetResponse(
+            Long id,
+            PetType petType,
+            String name,
+            String breed,
+            Integer age,
+            String color,
+            Float weight,
+            Long shelterId,
+            String shelterName) {
+
+        public PetResponse(Pet pet) {
+            this(pet.getId(), pet.getPetType(), pet.getName(), pet.getBreed(), pet.getAge(), pet.getColor(),
+                    pet.getWeight(), pet.getShelter().getId(), pet.getShelter().getName());
+        }
+    }
